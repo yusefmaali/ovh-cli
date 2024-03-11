8f078abd2d0e79c5441c850f4f5018f992da11bf
Yusef Maali <contact@yusefmaali.net>
2024-03-11 22:48:21 +0100

    refactor(zone_manager): renamed private members

--- ovh_cli/zone_manager.py
+++ ovh_cli/zone_manager.py
@@ -1,32 +1,34 @@
 import logging
 
 import ovh
+from ovh import ConsumerKeyRequest
 
 
 class ZoneManager:
-    logger: logging.Logger
-    client: ovh.Client
-    zone_name: str
-    domains: dict
+    _logger: logging.Logger
+    _client: ovh.Client
+    _zone_name: str
+    _domains: dict
+
 
     def __init__(self, zone_name: str):
-        self.logger = logging.getLogger("ZoneManager")
-        self.client = ovh.Client()
-        self.zone_name = zone_name
-        self.domains = {}
+        self._logger = logging.getLogger("ZoneManager")
+        self._client = ovh.Client()
+        self._zone_name = zone_name
+        self._domains = {}
 
         self._cache_all_domains()
 
     def domain_exists(self, domain_name: str) -> bool:
         """ Check if a domain exists in the zone """
-        return domain_name in self.domains
+        return domain_name in self._domains
 
     def get_hostname_ips(self, host_name: str) -> tuple:
         """ Get the ip addresses of the hostname """
         ipv4_address = None
         ipv6_address = None
 
-        records = self.domains.get(host_name)
+        records = self._domains.get(host_name)
         if records is None:
             return ipv4_address, ipv6_address
 
@@ -42,7 +44,7 @@ class ZoneManager:
 
     def dump_all_domains(self):
         """ Dump all domains"""
-        keys = list(self.domains.keys())
+        keys = list(self._domains.keys())
 
         super_group_keys = []
         for key in keys:
@@ -59,39 +61,39 @@ class ZoneManager:
             super_groups[key] = {}
 
             keys.remove(key)
-            super_groups[key][key] = self.domains.get(key)
+            super_groups[key][key] = self._domains.get(key)
 
             keys.remove(api_key)
-            super_groups[key][api_key] = self.domains.get(api_key)
+            super_groups[key][api_key] = self._domains.get(api_key)
 
         super_group_keys = list(super_groups.keys())
         super_group_keys.sort()
 
-        self.logger.info('Grouped domains (%d items)', len(super_group_keys))
+        self._logger.info('Grouped domains (%d items)', len(super_group_keys))
         for domain in super_group_keys:
-            self.logger.info(str(domain).upper())
+            self._logger.info(str(domain).upper())
             for sub_domain in super_groups[domain]:
-                self.logger.info('   %s', sub_domain)
+                self._logger.info('   %s', sub_domain)
                 for entry in super_groups[domain][sub_domain]:
                     field_type = entry['fieldType']
                     target = entry['target']
-                    self.logger.info('      %s => %s', field_type, target)
-            self.logger.info('')
+                    self._logger.info('      %s => %s', field_type, target)
+            self._logger.info('')
 
-        self.logger.info('')
-        self.logger.info('Other domains')
+        self._logger.info('')
+        self._logger.info('Other domains')
         for domain in keys:
             if len(domain) == 0:
-                self.logger.info("(empty)")
+                self._logger.info("(empty)")
             else:
-                self.logger.info(domain)
+                self._logger.info(domain)
 
-            for entry in self.domains[domain]:
+            for entry in self._domains[domain]:
                 field_type = entry['fieldType']
                 target = entry['target']
-                self.logger.info('      %s => %s', field_type, target)
+                self._logger.info('      %s => %s', field_type, target)
 
-            self.logger.info('')
+            self._logger.info('')
 
     def add_domain(self, domain_name: str, ipv4_address: str, ipv6_address: str, add_api_domain: bool):
         """ Add a test domain
@@ -106,7 +108,7 @@ class ZoneManager:
             api_domain_name = f'api.{domain_name}'
             self._add_domain_action(api_domain_name, ipv4_address, ipv6_address)
 
-        self.client.post(f'/domain/zone/{self.zone_name}/refresh')
+        self._client.post(f'/domain/zone/{self._zone_name}/refresh')
 
     def _add_domain_action(self, domain_name: str, ipv4_address: str, ipv6_address: str):
         """ Call the action to add a domain.
@@ -119,14 +121,14 @@ class ZoneManager:
             return
 
         if ipv4_address is not None:
-            self.logger.info('Adding domain (A) %s => %s', domain_name, ipv4_address)
-            self.client.post(f'/domain/zone/{self.zone_name}/record',
-                             fieldType='A', subDomain=domain_name, target=ipv4_address)
+            self._logger.info('Adding domain (A) %s => %s', domain_name, ipv4_address)
+            self._client.post(f'/domain/zone/{self._zone_name}/record',
+                              fieldType='A', subDomain=domain_name, target=ipv4_address)
 
         if ipv6_address is not None:
-            self.logger.info('Adding domain (AAAA) %s => %s', domain_name, ipv6_address)
-            self.client.post(f'/domain/zone/{self.zone_name}/record',
-                             fieldType='AAAA', subDomain=domain_name, target=ipv6_address)
+            self._logger.info('Adding domain (AAAA) %s => %s', domain_name, ipv6_address)
+            self._client.post(f'/domain/zone/{self._zone_name}/record',
+                              fieldType='AAAA', subDomain=domain_name, target=ipv6_address)
 
     def delete_domain(self, domain_name: str, ipv4_address: str, ipv6_address: str, delete_api_domain: bool):
         """ Delete a domain
@@ -141,7 +143,7 @@ class ZoneManager:
             api_domain_name = f'api.{domain_name}'
             self._delete_domain_action(api_domain_name, ipv4_address, ipv6_address)
 
-        self.client.post(f'/domain/zone/{self.zone_name}/refresh')
+        self._client.post(f'/domain/zone/{self._zone_name}/refresh')
 
     def _delete_domain_action(self, domain_name: str, ipv4_address: str, ipv6_address: str):
         """ Call the action to delete a domain.
@@ -150,7 +152,7 @@ class ZoneManager:
         :param ipv4_address
         :param ipv6_address
         """
-        records = self.domains.get(domain_name)
+        records = self._domains.get(domain_name)
         if records is None:
             return
 
@@ -159,30 +161,30 @@ class ZoneManager:
             target = record['target']
             domain_id = record['domainId']
             if (field_type == 'A' and target == ipv4_address) or (field_type == 'AAAA' and target == ipv6_address):
-                self.logger.info('Deleting domain (%s) %s, id: %s', field_type, domain_name, domain_id)
-                self.client.delete(f'/domain/zone/{self.zone_name}/record/{domain_id}')
+                self._logger.info('Deleting domain (%s) %s, id: %s', field_type, domain_name, domain_id)
+                self._client.delete(f'/domain/zone/{self._zone_name}/record/{domain_id}')
 
     def _cache_all_domains(self):
         """ Fetch all domains"""
-        response = self.client.get(f'/domain/zone/{self.zone_name}/record')
+        response = self._client.get(f'/domain/zone/{self._zone_name}/record')
 
-        self.domains = {}
+        self._domains = {}
 
         for domain_id in response:
-            domain = self.client.get(f'/domain/zone/{self.zone_name}/record/{domain_id}')
+            domain = self._client.get(f'/domain/zone/{self._zone_name}/record/{domain_id}')
             field_type = domain['fieldType']
             sub_domain = domain['subDomain']
             target = domain['target']
             domain_id = domain['id']
 
-            if sub_domain not in self.domains:
-                self.domains[sub_domain] = []
+            if sub_domain not in self._domains:
+                self._domains[sub_domain] = []
 
-            self.domains[sub_domain].append({
+            self._domains[sub_domain].append({
                 'fieldType': f'{field_type}',
                 'target': target,
                 'domainId': domain_id
             })
 
-        for domain in self.domains:
-            self.domains[domain].sort(key=lambda o: o['fieldType'])
+        for domain in self._domains:
+            self._domains[domain].sort(key=lambda o: o['fieldType'])
